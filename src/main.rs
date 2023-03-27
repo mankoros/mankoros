55104aadb473cbe3d7dc8ab2cd0a8e3dafbf273b
Origami404 <Origami404@foxmail.com>
2023-03-27 19:48:24 +0800

    [tools][feat] add handler_pool for int handler management

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ mod syscall;
 mod utils;
 #[macro_use]
 mod xdebug;
+mod tools;
 
 use driver::uart::Uart;
 use log::{error, info};
---
+++ src/tools/handler_pool.rs
@@ -0,0 +1,29 @@
+use alloc::vec::Vec;
+
+pub struct UsizePool {
+    next: usize,
+    recycled: Vec<usize>,
+}
+
+impl UsizePool {
+    pub const fn new() -> Self {
+        UsizePool {
+            next: 0,
+            recycled: Vec::new(),
+        }
+    }
+
+    pub fn get(&mut self) -> usize {
+        if let Some(pid) = self.recycled.pop() {
+            pid
+        } else {
+            let pid = self.next;
+            self.next += 1;
+            pid
+        }
+    }
+
+    pub fn release(&mut self, pid: usize) {
+        self.recycled.push(pid);
+    }
+}
