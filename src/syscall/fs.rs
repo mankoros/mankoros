c5066a60e449d9cb95adde725c30a4163409ffe4
Easton Man <manyang.me@outlook.com>
2023-04-30 15:46:46 +0800

    [syscall][feat]: impl read

--- src/syscall/fs.rs
+++ src/syscall/fs.rs
@@ -14,7 +14,7 @@ use super::{Syscall, SyscallResult};
 
 impl<'a> Syscall<'a> {
     pub fn sys_write(&mut self, fd: usize, buf: *const u8, len: usize) -> SyscallResult {
-        info!("Syscall: write, sys_write fd {fd}");
+        info!("Syscall: write, fd {fd}");
         self.process.with_alive(|a| {
             let mut fds = a.get_file_descripter().clone();
 
@@ -37,9 +37,29 @@ impl<'a> Syscall<'a> {
             Ok(write_len)
         })
     }
-    pub fn sys_read(&mut self, _fd: usize, _buf: *mut u8, _len: usize) -> SyscallResult {
-        todo!();
-        Ok(0)
+    pub fn sys_read(&mut self, fd: usize, buf: *mut u8, len: usize) -> SyscallResult {
+        info!("Syscall: read, fd {fd}");
+        self.process.with_alive(|a| {
+            let mut fds = a.get_file_descripter().clone();
+
+            // Sanity check
+            if fd >= fds.len() {
+                return Err(AxError::InvalidInput);
+            }
+            // Convert user vaddr
+            // TODO: do not panic when invalid vaddr
+            let paddr = a.get_user_space().page_table.get_paddr_from_vaddr((buf as usize).into());
+
+            let kernel_vaddr = kernel_phys_to_virt(paddr.into());
+
+            let file = &mut fds[fd];
+
+            let read_len = file.read_at(0, unsafe {
+                core::slice::from_raw_parts_mut(kernel_vaddr as *mut u8, len)
+            })?;
+
+            Ok(read_len)
+        })
     }
 
     pub fn sys_openat(
