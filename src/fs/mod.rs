8a8f474d6432ed8a29504a671bf6217e8a1c3401
Easton Man <manyang.me@outlook.com>
2023-04-30 19:11:27 +0800

    [fs][fix]: fix path parsing, pass BIST

--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -19,6 +19,9 @@ pub mod vfs;
 type BlockDevice = driver::VirtIoBlockDev;
 
 pub fn init_filesystems(blk_dev: BlockDevice) {
+    info!("Filesystem built-in self testing (BIST)...");
+    vfs::path::path_test();
+
     info!("Initialize filesystems...");
     info!("  use block device: {:?}", blk_dev.device_name());
 
--- src/fs/vfs/path.rs
+++ src/fs/vfs/path.rs
@@ -1,22 +1,25 @@
-// From Oops: utils/path.rs 
+// From Oops: utils/path.rs
+use alloc::format;
+use alloc::{collections::VecDeque, string::String};
 use core::fmt::{Debug, Formatter};
 use core::ops::{Deref, DerefMut};
-use alloc::{
-    collections::VecDeque,
-    string::String,
-};
-use alloc::format;
 use log::debug;
 
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Path {
+    pub is_absolute: bool,
     pub components: VecDeque<String>,
 }
 
 impl Debug for Path {
     fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
-        write!(f, "/")?;
-        for p in &self.components {
+        if self.is_absolute {
+            write!(f, "/")?;
+        }
+        for (idx, p) in self.components.iter().enumerate() {
+            if idx > 0 {
+                write!(f, "/")?;
+            }
             write!(f, "{}", p)?;
         }
         Ok(())
@@ -37,42 +40,33 @@ impl Deref for Path {
     }
 }
 
-impl DerefMut for Path {    
+impl DerefMut for Path {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.components
     }
 }
 
-
 impl Path {
     pub fn from_string(path: String) -> Result<Self, ()> {
-        let mut temp: VecDeque<String> = path.split('/').map(|s| String::from(s)).collect();
-
-        if path.starts_with('/') {
-            temp.pop_front();
-        }
-        if path.starts_with("//") {
-            temp.pop_front();
-        }
-        if path.ends_with('/') {
-            temp.pop_back();
-        }
-
+        let is_absolute = path.starts_with('/');
         let mut components = VecDeque::new();
-        for name in temp {
-            if name == "." {
-                continue;
-            } else if name == ".." {
-                let ret = components.pop_back();
-                if ret.is_none() {
-                    return Err(());
+        for part in path.split('/') {
+            match part {
+                "" | "." => continue,
+                ".." => {
+                    if !components.is_empty() {
+                        components.pop_back().unwrap();
+                    }
                 }
-            } else {
-                components.push_back(name);
+                _ => {
+                    components.push_back(String::from(part));
+                }
             }
-        } 
+        }
+
         Ok(Self {
-            components
+            components,
+            is_absolute,
         })
     }
 
@@ -86,24 +80,28 @@ impl Path {
 
     /// Whether it is the root
     pub fn is_root(&self) -> bool {
-        return self.components.len() == 0;
+        self.is_absolute && self.components.is_empty()
     }
 
     /// Get the tail of the path
     pub fn last(&self) -> &String {
-        if self.is_root() {panic!("is_root")}
-        return &self.components[self.len() - 1]
+        if self.is_root() {
+            panic!("is_root")
+        }
+        return &self.components[self.len() - 1];
     }
 
     #[allow(unused)]
     pub fn first(&self) -> &String {
-        return &self.components[0]
+        return &self.components[0];
     }
 
     /// Remove the head of the path
     #[allow(unused)]
     pub fn remove_head(&self) -> Self {
-        if self.is_root() {panic!("already root")}
+        if self.is_root() {
+            panic!("already root")
+        }
         let mut new = self.clone();
         new.pop_front();
         new
@@ -111,12 +109,14 @@ impl Path {
 
     /// Remove the tail of the path
     pub fn remove_tail(&self) -> Self {
-        if self.is_root() {panic!("already root")}
+        if self.is_root() {
+            panic!("already root")
+        }
         let mut new = self.clone();
         new.pop_back();
         new
     }
-    
+
     pub fn without_prefix(&self, prefix: &Path) -> Self {
         assert!(self.starts_with(prefix), "not prefix");
         let mut new = self.clone();
@@ -146,15 +146,149 @@ impl Path {
 #[allow(unused)]
 pub fn path_test() {
     let path = Path::from_string(String::from("/a/b/c/d/")).unwrap();
-    debug!("path = {:?}", path);
+    debug_assert_eq!(path.to_string(), "/a/b/c/d");
     let path = Path::from_string(String::from("/abcdefg/asdsd/asdasd")).unwrap();
-    debug!("path = {:?}", path);
+    debug_assert_eq!(path.to_string(), "/abcdefg/asdsd/asdasd");
     let path = Path::from_string(String::from("aa/../bb/../cc/././."));
-    debug!("path = {:?}", path);
-    let path = Path::from_string(String::from("aa/../.."));
-    debug!("path = {:?}", path);
-    let path = Path::from_string(String::from("./././."));
-    debug!("path = {:?}", path);
-    //todo!()
-    //println!("{:?}", path.components);
+    debug_assert!(path.is_ok());
+    debug_assert_eq!(path.unwrap().to_string(), "cc");
+
+    debug_assert_eq!(
+        Path::from_string(String::from("///")).unwrap().to_string(),
+        "/"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("//a//.//b///c//")).unwrap().to_string(),
+        "/a/b/c"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/a/../")).unwrap().to_string(),
+        "/"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("a/../")).unwrap().to_string(),
+        ""
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("a/..//..")).unwrap().to_string(),
+        ""
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("././a")).unwrap().to_string(),
+        "a"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from(".././a")).unwrap().to_string(),
+        "a"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/././a")).unwrap().to_string(),
+        "/a"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/abc/../abc")).unwrap().to_string(),
+        "/abc"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/test")).unwrap().to_string(),
+        "/test"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/test/")).unwrap().to_string(),
+        "/test"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("test/")).unwrap().to_string(),
+        "test"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("test")).unwrap().to_string(),
+        "test"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/test//")).unwrap().to_string(),
+        "/test"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/test/foo")).unwrap().to_string(),
+        "/test/foo"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/test/foo/")).unwrap().to_string(),
+        "/test/foo"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/test/foo/bar")).unwrap().to_string(),
+        "/test/foo/bar"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/test/foo/bar//")).unwrap().to_string(),
+        "/test/foo/bar"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/test//foo/bar//")).unwrap().to_string(),
+        "/test/foo/bar"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/test//./foo/bar//")).unwrap().to_string(),
+        "/test/foo/bar"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/test//./.foo/bar//")).unwrap().to_string(),
+        "/test/.foo/bar"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/test//./..foo/bar//")).unwrap().to_string(),
+        "/test/..foo/bar"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/test//./../foo/bar//")).unwrap().to_string(),
+        "/foo/bar"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/test/../foo")).unwrap().to_string(),
+        "/foo"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/test/bar/../foo")).unwrap().to_string(),
+        "/test/foo"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("../foo")).unwrap().to_string(),
+        "foo"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("../foo/")).unwrap().to_string(),
+        "foo"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/../foo")).unwrap().to_string(),
+        "/foo"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/../foo/")).unwrap().to_string(),
+        "/foo"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/../../foo")).unwrap().to_string(),
+        "/foo"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/bleh/../../foo")).unwrap().to_string(),
+        "/foo"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/bleh/bar/../../foo")).unwrap().to_string(),
+        "/foo"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/bleh/bar/../../foo/..")).unwrap().to_string(),
+        "/"
+    );
+    debug_assert_eq!(
+        Path::from_string(String::from("/bleh/bar/../../foo/../meh"))
+            .unwrap()
+            .to_string(),
+        "/meh"
+    );
 }
