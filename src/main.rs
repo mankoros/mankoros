a5649a124a96751e8491cc2e17893340152c7ac6
Easton Man <manyang.me@outlook.com>
2023-05-23 17:48:05 +0800

    [test][feat]: add test to main

--- src/main.rs
+++ src/main.rs
@@ -227,6 +227,8 @@ pub extern "C" fn boot_rust_main(boot_hart_id: usize, _device_tree_addr: usize) 
                 "close",
                 "mmap",
                 "munmap",
+                "gettimeofday",
+                "times",
             ];
         }
     }
