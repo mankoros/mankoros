c89d09534284ec545f54c76e5323f6badfd7c835
Origami404 <Origami404@foxmail.com>
2023-07-02 01:18:22 +0800

    [sync][feat] export guard type of locks

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -2,4 +2,7 @@ mod mutex;
 mod sleep;
 
 pub type SpinNoIrqLock<T> = mutex::Mutex<T, mutex::SpinNoIrq>;
+pub type SpinNoIrqLockGuard<'a, T> = mutex::MutexGuard<'a, T, mutex::SpinNoIrq>;
 pub type SleepLock<T> = sleep::SleepLock<T>;
+pub type SleepLockFuture<'a, T> = sleep::SleepLockFuture<'a, T>;
+pub type SleepLockGuard<'a, T> = sleep::SleepLockGuard<'a, T>;
