7abd0a46fafca075fbb38ec23f6e7786ad2c1fcd
Origami404 <Origami404@foxmail.com>
2023-07-06 16:20:30 +0800

    [async][feat] add AnyFuture

--- src/executor/util_futures.rs
+++ src/executor/util_futures.rs
@@ -41,3 +41,48 @@ impl<F: Future> Future for SumFuture<F> {
 pub fn within_sum_async<F: Future>(future: F) -> SumFuture<F> {
     SumFuture { future }
 }
+
+pub struct AnyFuture<'a, T> {
+    futures: Vec<Async<'a, T>>,
+    has_returned: bool,
+}
+
+impl<'a, T> AnyFuture<'a, T> {
+    pub fn new() -> Self {
+        Self {
+            futures: Vec::new(),
+            has_returned: false,
+        }
+    }
+    pub fn push(&mut self, future: Async<'a, T>) {
+        self.futures.push(future);
+    }
+
+    pub fn new_with(futures: Vec<Async<'a, T>>) -> Self {
+        Self {
+            futures,
+            has_returned: false,
+        }
+    }
+}
+
+impl<T> Future for AnyFuture<'_, T> {
+    type Output = (usize, T);
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = unsafe { self.get_unchecked_mut() };
+        if this.has_returned {
+            return Poll::Pending;
+        }
+
+        for (i, future) in this.futures.iter_mut().enumerate() {
+            let result = unsafe { Pin::new_unchecked(future).poll(cx) };
+            if let Poll::Ready(ret) = result {
+                this.has_returned = true;
+                return Poll::Ready((i, ret));
+            }
+        }
+
+        Poll::Pending
+    }
+}
