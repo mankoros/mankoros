a90b4f9f6f4af5287b70aa3cda4d2fe9a5ac9131
Origami404 <Origami404@foxmail.com>
2023-08-03 10:25:33 +0800

    [trap][feat] add user rw check mode

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -5,3 +5,16 @@ pub mod hash;
 pub mod pointers;
 pub mod sync_ptr;
 pub mod user_check;
+
+#[macro_export]
+macro_rules! when_debug {
+    ($blk:block) => {
+        cfg_if::cfg_if! {
+            if #[cfg(debug_assertions)] {
+                $blk
+            }
+        }
+    };
+}
+
+pub use when_debug;
--- src/trap/trap.rs
+++ src/trap/trap.rs
@@ -1,5 +1,5 @@
-use crate::arch;
 use crate::trap::context::UKContext;
+use crate::{arch, when_debug};
 use core::arch::global_asm;
 use riscv::register::sstatus;
 use riscv::register::{stvec, utvec::TrapMode};
@@ -11,6 +11,7 @@ global_asm!(include_str!("trap.asm"));
 pub fn init() {
     unsafe {
         set_kernel_trap();
+        enable_irq();
     }
 }
 
@@ -24,6 +25,7 @@ pub fn run_user(cx: &mut UKContext) {
         set_user_trap();
         __kernel_to_user(cx as *mut _ as *mut _);
         set_kernel_trap();
+        enable_irq();
     }
 }
 
@@ -47,12 +49,62 @@ unsafe fn set_kernel_trap() {
     );
     let trap_vaddr = __kernel_trap_vector as usize;
     stvec::write(trap_vaddr, TrapMode::Vectored);
-    // Enable irq
-    sstatus::set_sie();
 
     trace!(
         "Interrupts enabled for hart {} at STVEC: 0x{:x}",
         arch::get_hart_id(),
         trap_vaddr
     );
+}
+
+#[inline(always)]
+unsafe fn enable_irq() {
+    sstatus::set_sie();
+}
+
+#[inline(always)]
+unsafe fn set_kernel_user_rw_trap() {
+    extern "C" {
+        fn __user_rw_trap_entry();
+    }
+    let trap_vaddr = __user_rw_trap_entry as usize;
+    stvec::write(trap_vaddr, TrapMode::Vectored);
+    trace!(
+        "Switch to User-RW checking mode for hart {} at STVEC: 0x{:x}",
+        arch::get_hart_id(),
+        trap_vaddr
+    );
+}
+
+#[inline(always)]
+fn will_read_fail(vaddr: usize) -> bool {
+    when_debug!({
+        extern "C" {
+            fn __user_rw_trap_entry();
+        }
+        let curr_stvec = stvec::read().address();
+        debug_assert!(curr_stvec == __user_rw_trap_entry as usize);
+    });
+
+    extern "C" {
+        fn __try_read_user(vaddr: usize) -> bool;
+    }
+
+    unsafe { __try_read_user(vaddr) }
+}
+
+#[inline(always)]
+fn will_write_fail(vaddr: usize) -> bool {
+    when_debug!({
+        extern "C" {
+            fn __user_rw_trap_entry();
+        }
+        let curr_stvec = stvec::read().address();
+        debug_assert!(curr_stvec == __user_rw_trap_entry as usize);
+    });
+
+    extern "C" {
+        fn __try_write_user(vaddr: usize) -> bool;
+    }
+    unsafe { __try_write_user(vaddr) }
 }
