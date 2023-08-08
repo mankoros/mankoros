a4918424edee1f524b0733bf95dce5160cb72a39
Origami404 <Origami404@foxmail.com>
2023-08-08 14:53:50 +0800

    [fs][feat] add /tmp & support create

--- src/fs/memfs/tmpdir.rs
+++ src/fs/memfs/tmpdir.rs
@@ -3,7 +3,7 @@ use crate::{
         top::{VfsFile, VfsFileRef},
         DeviceIDCollection, VfsFileAttr, VfsFileKind,
     },
-    here, impl_vfs_default_non_file,
+    here, impl_vfs_default_non_dir, impl_vfs_default_non_file,
     sync::SpinNoIrqLock,
     tools::errors::{dyn_future, ASysResult, SysError},
 };
@@ -12,6 +12,105 @@ use alloc::{
     string::{String, ToString},
     vec::Vec,
 };
+
+pub struct TmpFile {
+    content: SpinNoIrqLock<Vec<u8>>,
+}
+
+impl TmpFile {
+    pub fn new() -> Self {
+        Self {
+            content: SpinNoIrqLock::new(Vec::new()),
+        }
+    }
+}
+
+impl VfsFile for TmpFile {
+    fn attr(&self) -> ASysResult<VfsFileAttr> {
+        dyn_future(async {
+            Ok(VfsFileAttr {
+                kind: VfsFileKind::RegularFile,
+                device_id: DeviceIDCollection::TMP_FS_ID,
+                self_device_id: 0,
+                byte_size: self.content.lock(here!()).len(),
+                block_count: 0,
+                access_time: 0,
+                modify_time: 0,
+                create_time: 0,
+            })
+        })
+    }
+
+    fn read_at<'a>(&'a self, offset: usize, buf: &'a mut [u8]) -> ASysResult<usize> {
+        dyn_future(async move {
+            let content = self.content.lock(here!());
+            let len = core::cmp::min(buf.len(), content.len() - offset);
+            buf[..len].copy_from_slice(&content[offset..offset + len]);
+            Ok(len)
+        })
+    }
+
+    fn write_at<'a>(&'a self, offset: usize, buf: &'a [u8]) -> ASysResult<usize> {
+        dyn_future(async move {
+            let mut content = self.content.lock(here!());
+
+            if content.len() < offset {
+                content.resize(offset, 0);
+            }
+
+            // content.len() >= offset
+            let in_content_len = core::cmp::min(buf.len(), content.len() - offset);
+            for i in 0..in_content_len {
+                content[i + offset] = buf[i];
+            }
+
+            let out_content_len = buf.len() - in_content_len;
+            for i in 0..out_content_len {
+                content.push(buf[i + in_content_len]);
+            }
+
+            Ok(buf.len())
+        })
+    }
+
+    fn get_page(
+        &self,
+        _offset: usize,
+        _kind: crate::fs::new_vfs::top::MmapKind,
+    ) -> ASysResult<crate::memory::address::PhysAddr4K> {
+        unimplemented!("mmap for tmpfile")
+    }
+
+    fn truncate(&self, length: usize) -> ASysResult {
+        dyn_future(async move {
+            self.content.lock(here!()).resize(length, 0);
+            Ok(())
+        })
+    }
+
+    fn poll_ready(
+        &self,
+        _offset: usize,
+        _len: usize,
+        _kind: crate::fs::new_vfs::top::PollKind,
+    ) -> ASysResult<usize> {
+        unimplemented!("poll for tmpfile")
+    }
+
+    fn poll_read(&self, _offset: usize, _buf: &mut [u8]) -> usize {
+        unimplemented!("poll for tmpfile")
+    }
+
+    fn poll_write(&self, _offset: usize, _buf: &[u8]) -> usize {
+        unimplemented!("poll for tmpfile")
+    }
+
+    impl_vfs_default_non_dir!(TmpFile);
+
+    fn as_any(&self) -> &dyn core::any::Any {
+        self
+    }
+}
 
 pub struct TmpDir {
     children: SpinNoIrqLock<BTreeMap<String, VfsFileRef>>,
@@ -43,8 +142,25 @@ impl VfsFile for TmpDir {
         })
     }
 
-    fn create<'a>(&'a self, _name: &'a str, _kind: VfsFileKind) -> ASysResult<VfsFileRef> {
-        unimplemented!("TmpDir::create")
+    fn create<'a>(&'a self, name: &'a str, kind: VfsFileKind) -> ASysResult<VfsFileRef> {
+        dyn_future(async move {
+            let mut children = self.children.lock(here!());
+
+            if children.contains_key(name) {
+                return Err(SysError::EEXIST);
+            }
+
+            let new_file = match kind {
+                VfsFileKind::Directory => VfsFileRef::new(Self::new()),
+                VfsFileKind::RegularFile => VfsFileRef::new(TmpFile::new()),
+                _ => panic!("unknown kind"),
+            };
+
+            let ret = children.insert(name.to_string(), new_file.clone());
+            debug_assert!(ret.is_none());
+
+            Ok(new_file)
+        })
     }
 
     fn lookup<'a>(&'a self, name: &'a str) -> ASysResult<VfsFileRef> {
